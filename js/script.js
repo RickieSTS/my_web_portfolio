3268f68f664f487ba703cdfc5eb346431ef317ba
Tekku <testmail@tekku.pt>
2024-10-31 18:37:13 +0000

    Fixed nav theme button spacing Added new animations to navbar items started burger modal implementation

--- js/script.js
+++ js/script.js
@@ -1,71 +1,80 @@
-
 // document.cookie = "theme=light; expires=31 Dec 2026 12:00:00 UTC; path=/";
 
-function checkCookieTheme(){
+function checkCookieTheme() {
   console.log(document.cookie);
-  const body = document.getElementById("body"); 
-  body.style.visibility="visible";
-    if(document.cookie == "theme=dark")
-    {
+  const body = document.getElementById("body");
+  body.style.visibility = "visible";
+  if (document.cookie == "theme=dark") {
     // alert("dark");
     body.classList.remove("light");
-        body.classList.add("dark");
-        var x = document.getElementById("darkThemeButtonLabel").style.display = "none";
-        var y = document.getElementById("lightThemeButtonLabel").style.display = "block";
-    }
-    else{
+    body.classList.add("dark");
+    var x = (document.getElementById("darkThemeButtonLabel").style.display =
+      "none");
+    var y = (document.getElementById("lightThemeButtonLabel").style.display =
+      "block");
+  } else {
     // alert("light");
     body.classList.remove("dark");
-        body.classList.add("light");
-        var x = document.getElementById("darkThemeButtonLabel").style.display = "block";
-        var y = document.getElementById("lightThemeButtonLabel").style.display = "none";
-    }
+    body.classList.add("light");
+    var x = (document.getElementById("darkThemeButtonLabel").style.display =
+      "block");
+    var y = (document.getElementById("lightThemeButtonLabel").style.display =
+      "none");
+  }
 }
 
-function setTheme(){
-
+function setTheme() {
   // alert(document.cookie)
   // let cookieThemeValue = getCookie("theme");
-  
 
   var selectedTheme;
   // console.log("started");
-   const radioList = document.getElementsByName("theme");
-   const body = document.getElementById("body"); 
+  const radioList = document.getElementsByName("theme");
+  const body = document.getElementById("body");
 
   //  console.log(body);
-     radioList.forEach(item => {
-      // console.log(item);
-      
+  radioList.forEach((item) => {
+    // console.log(item);
 
-      if(item.checked == true)
-      {
-        // console.log(item.id)
-        selectedTheme = item.id;
-      }
-    });
-
-    switch(selectedTheme){
-      default:
-      case "lightThemeButton":
-        body.classList.remove("dark");
-        body.classList.add("light");
-        var x = document.getElementById("darkThemeButtonLabel").style.display = "block";
-        var y = document.getElementById("lightThemeButtonLabel").style.display = "none";
-        document.cookie = "theme=light; expires=31 Dec 2026 12:00:00 UTC; path=/";
-
-      break;
-      case "darkThemeButton":
-        body.classList.remove("light");
-        body.classList.add("dark");
-        var x = document.getElementById("darkThemeButtonLabel").style.display = "none";
-        var y = document.getElementById("lightThemeButtonLabel").style.display = "block";
-        document.cookie = "theme=dark; expires=31 Dec 2026 12:00:00 UTC; path=/";
-
-      break;
-      
+    if (item.checked == true) {
+      // console.log(item.id)
+      selectedTheme = item.id;
     }
-    
+  });
 
-  
+  switch (selectedTheme) {
+    default:
+    case "lightThemeButton":
+      body.classList.remove("dark");
+      body.classList.add("light");
+      var x = (document.getElementById("darkThemeButtonLabel").style.display =
+        "block");
+      var y = (document.getElementById("lightThemeButtonLabel").style.display =
+        "none");
+      document.cookie = "theme=light; expires=31 Dec 2026 12:00:00 UTC; path=/";
+
+      break;
+    case "darkThemeButton":
+      body.classList.remove("light");
+      body.classList.add("dark");
+      var x = (document.getElementById("darkThemeButtonLabel").style.display =
+        "none");
+      var y = (document.getElementById("lightThemeButtonLabel").style.display =
+        "block");
+      document.cookie = "theme=dark; expires=31 Dec 2026 12:00:00 UTC; path=/";
+
+      break;
+  }
 }
+let modal = document.querySelector("dialog");
+let burgerButton = document.querySelector(".burger-button");
+
+burgerButton.addEventListener("click", () => {
+  modal.showModal();
+});
+
+modal.addEventListener("click", (event) => {
+  if (event.target.id !== 'dialog') {
+    modal.close();
+  }
+});
